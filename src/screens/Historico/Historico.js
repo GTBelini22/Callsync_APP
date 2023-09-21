cb9fdfbd68366a0960199e0d16c58940e3991489
AllexFelicio <junioralex106@gmail.com>
2023-09-20 23:41:36 -0300

    tela historico

--- src/screens/Historico/Historico.js
+++ src/screens/Historico/Historico.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, Button, FlatList, Text } from 'react-native';
+import { View, TextInput, Button, FlatList, Text, TouchableOpacity, ScrollView } from 'react-native';
 import { app, firestore } from '../../config/Firebase' // Verifique a importação
 import { styles } from './styles'; // Importe o estilo
 import {collection, getDocs, query, where} from 'firebase/firestore'
@@ -12,7 +12,6 @@ const Historico = () => {
     try {
       const usersRef = query(collection(firestore,'users'));
       const resultado =  await getDocs(usersRef); // Use firestore assim
-      console.log(resultado.docs);
       const searchData = resultado.docs.map((doc) => doc.data());
       setData(searchData);
     } catch (error) {
@@ -36,28 +35,50 @@ const Historico = () => {
     }
   };
 
-  return (
+  return ( 
+    <>
     <View>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Digite sua pesquisa"
-          value={searchTerm}
-          onChangeText={(text) => setSearchTerm(text)}
-        />
-        <Button title="Pesquisar" onPress={handleSearch} />
-      </View>
-      <View style={styles.line} />
-      <FlatList
-        data={data}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          <Text>
-            {item.nome} - {item.email}
-          </Text>
-        )}
-      />
-    </View>
+  <View style={styles.inputContainer}>
+    <TextInput
+      style={styles.input}
+      placeholder="Digite sua pesquisa"
+      value={searchTerm}
+      onChangeText={(text) => setSearchTerm(text)}
+    />
+    <Button title="Pesquisar" onPress={handleSearch} />
+  </View>
+  <View style={styles.line} />
+  <View style={styles.container}>
+  <FlatList
+    data={data}
+    keyExtractor={(item, index) => index.toString()}
+    renderItem={({ item }) => (
+      <TouchableOpacity
+  style={styles.itemContainer}
+  onPress={() => {
+    // Ação a ser realizada quando um item for pressionado
+    // Por exemplo, você pode navegar para outra tela ou executar alguma ação específica aqui
+  }}
+>
+  <View style={styles.itemFlat}>
+    <Text>
+      {item.nome}
+    </Text>
+    <Text>
+   {item.email}
+    </Text>
+  </View>
+</TouchableOpacity>
+    )}
+  />
+</View>
+</View>
+<View style={styles.containerButton}>
+<TouchableOpacity style={styles.roundedButton}>
+        <Text style={styles.buttonText}>Novo Chamado</Text>
+      </TouchableOpacity>
+</View>
+</>
   );
 };
 
